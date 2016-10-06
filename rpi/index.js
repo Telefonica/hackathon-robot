d93a2de25b843fea7a8c8bc578ec81e68aa1c23f
Guido García <guido.garciabernardo@telefonica.com>
2016-10-06 12:09:22 +0200

    serial write

--- rpi/index.js
+++ rpi/index.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var http = require('http');
+var SerialPort = require('serialport');
 
 var app = express();
 var server = http.createServer(app);
 
 // XXX a websocket connection could work
 
+var serialDevice = process.env.SERIAL_DEVICE || '/dev/tty-usbserial1';
+var serial = new SerialPort(serialDevice, {
+    baudRate: 9600
+});
+
 app.get('/move/:button', function(req, res) {
-    console.log(req.params.button);
-    res.send('ok');
+    var button = req.params.button;
+    res.send('ok ' + button);
+    serial.write(button);
+});
+
+process.on('uncaughtException', function (err) {
+    console.error(err);
+    process.exit(1);
 });
 
 app.listen(3000);
