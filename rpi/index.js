e57efaa032240195434fbac2b08cefb37c9e10b5
Guido García <guido.garciabernardo@telefonica.com>
2016-10-06 12:25:33 +0200

    error handling

--- rpi/index.js
+++ rpi/index.js
@@ -5,17 +5,43 @@ var SerialPort = require('serialport');
 var app = express();
 var server = http.createServer(app);
 
-// XXX a websocket connection could work
+// XXX a websocket connection could work better
+var MOVES = {
+    'up': 1,
+    'left': 2,
+    'rigth': 3,
+    'down': 4
+};
+
+app.get('/move/:button', function(req, res) {
+    var button = req.params.button;
+
+    var move = MOVES[button];
+    if (!code) {
+        return res.status(400).send('fail wrong input');
+    }
+
+    serial.write(move, function(err) {
+        if (err) {
+            console.err('write to serial error', err);
+            return res.status(500).send('fail communications');
+        }
+
+        res.send('ok ' + button);
+    });
+});
 
 var serialDevice = process.env.SERIAL_DEVICE || '/dev/tty-usbserial1';
 var serial = new SerialPort(serialDevice, {
     baudRate: 9600
 });
 
-app.get('/move/:button', function(req, res) {
-    var button = req.params.button;
-    res.send('ok ' + button);
-    serial.write(button);
+serial.on('open', function() {
+    console.log('serial port open');
+});
+
+serial.on('error', function(err) {
+    console.error('serial port error', err);
 });
 
 process.on('uncaughtException', function (err) {
