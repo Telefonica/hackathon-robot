031457e27e6fc609383ce104872b1c4ea0127ccf
Guido García <palmerabollo@gmail.com>
2016-10-06 14:42:04 +0000

    fix web

--- rpi/index.js
+++ rpi/index.js
@@ -13,6 +13,8 @@ var MOVES = {
     'down': '4'
 };
 
+app.use('/static', express.static(__dirname + '/../web/'));
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
