38a978df4b8d18168a201cbd9a6cfb6ddbf23655
Guido García <palmerabollo@gmail.com>
2016-10-06 14:50:29 +0000

    remove alerts

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -2,7 +2,7 @@ var speed = 5;
 const APIKEY = "45697832";
 const SESSIONID = "2_MX40NTY5NzgzMn5-MTQ3NTc2MTg4MDU2OH5oQjhUb0t0UUhYLzV4elV3ZWF5UGxSSk5-fg"; 
 const TOKEN = "T1==cGFydG5lcl9pZD00NTY5NzgzMiZzZGtfdmVyc2lvbj10YnBocC12MC45MS4yMDExLTA3LTA1JnNpZz1hYzMyMDk3YTkyODM4ZDY3YjdjYmQyZTg1NjY4OTczYjllYmIwMmEzOnNlc3Npb25faWQ9Ml9NWDQwTlRZNU56Z3pNbjUtTVRRM05UYzJNVGc0TURVMk9INW9RamhVYjB0MFVVaFlMelY0ZWxWM1pXRjVVR3hTU2s1LWZnJmNyZWF0ZV90aW1lPTE0NzU3NjE2Njkmcm9sZT1tb2RlcmF0b3Imbm9uY2U9MTQ3NTc2MTY2OS4wNjQyMTQ1NjQ0ODA5MyZleHBpcmVfdGltZT0xNDc4MzUzNjY5";
-const HOST = 'http://192.168.242.71';
+const HOST = 'http://192.168.242.71:3000';
 var active = {
 	up: false,
 	down: false, 
@@ -26,10 +26,10 @@ function moveTo(direction) {
 $(function() {
 	var session = OT.initSession(APIKEY, SESSIONID);
 	session.connect(TOKEN, function() {
-		alert('connected to Robot room');
+		// alert('connected to Robot room');
 	});
 	session.on('streamCreated', function (event) {
-		alert('connected to Robot video');
+		// alert('connected to Robot video');
 		session.subscribe(event.stream, 'videoDiv');
 	});
 
